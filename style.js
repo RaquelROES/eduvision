e23e0705f4745f3509b1b416ebf4d9aa57159e4c
RaquelROES <32135998+RaquelROES@users.noreply.github.com>
2018-08-03 10:14:50 +0200

    Update style.js

--- style.js
+++ style.js
@@ -31,7 +31,7 @@
 
 				{ name: 'Icono Aplico', element: 'span', attributes: { 'class': 'icon icon-aplico' } },
 				{ name: 'Icono Mapa conceptual', element: 'span', attributes: { 'class': 'icon icon-aprendamosconelmapaconceptual' } },
-				{ name: 'Icono Taller', element: 'span', attributes: { 'class': 'icon-taller' } },
+				{ name: 'Icono Taller', element: 'span', attributes: { 'class': 'icon icon-taller' } },
 				{ name: 'Icono Birrete', element: 'span', attributes: { 'class': 'icon icon-birrete' } },
 				{ name: 'Icono libro', element: 'span', attributes: { 'class': 'icon icon-libro' } },
 				{ name: 'Icono Lápiz', element: 'span', attributes: { 'class': 'icon icon-lapiz' } },
