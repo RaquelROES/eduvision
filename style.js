41ae9f589e02e00ed58f70aede65df5719b5d6f1
RaquelROES <32135998+RaquelROES@users.noreply.github.com>
2018-09-10 17:37:03 +0200

    Update style.js

--- style.js
+++ style.js
@@ -70,7 +70,7 @@
 				{ name: 'Caja Cuaderno', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-13' } },
 				{ name: 'Caja Sabías', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-14' } },
 				{ name: 'Caja Costumbres', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-15' } },
-				{ name: 'Caja Diccionario', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-15' } },
+				{ name: 'Caja Diccionario', type: 'widget', widget: 'blink_box', attributes: { 'class': 'box-16' } },
 			]
 		},
 
